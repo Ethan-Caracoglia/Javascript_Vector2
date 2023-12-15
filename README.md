f6d39d3fac2a17a8b6287de948d424d80969069c
Ethan-Caracoglia <ethancaracoglia@gmail.com>
2023-12-15 03:33:32 -0500

    Created Vector2.js and Updated README.md

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Javascript_Vector2
-A JavaScript file containing a Vector2 class. This is meant to be used for game development adjacent projects that require movement in 2 axis.
+
+## Description
+A JavaScript file containing a Vector2 class. This contains a basic vector2 class that contains 
+basic x and y properties as well as functions to add or subtract the vector with another vector. 
+It also contains scalar multiplying, magnitude, and normalized functions for working with the 
+magnitude and changing it. It does contain a function to find the angle in degrees as well as a 
+function to find the dot product and the distance of this and another vector2. The Vector2 type is 
+checked during any method that takes a Vector2 as a parameter.
+
+## Purpose
+This is meant to be used where vector movement and math is important and it would be desireable if 
+it was made much easier.
---
+++ Vector2.js
@@ -0,0 +1,89 @@
+'use strict'
+
+//Vector2 Class becasue I lost my mind
+class Vector2 {
+    constructor (x, y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    // Returns the magnitude of the current Vector2
+    magnitude() {
+        return Math.sqrt(
+            Math.pow(this.x, 2) + 
+            Math.pow(this.y, 2)
+            );
+    }
+
+    // Returns a normalized version of the current Vector2
+    normalized() {
+        return new Vector2(
+            this.x / this.magnitude(),
+            this.y / this.magnitude()
+        );
+    }
+
+    //Returns the counter-clockwise angle of the current Vector2 
+    angle() {
+        return Math.atan(this.y / this.x) * 
+        (180 / Math.PI);
+    }
+
+    // Returns the product of the current Vector2 and the scalar quantity.
+    multiply(scale) {
+        return new Vector2(
+            this.x * scale,
+            this.y * scale
+        );
+    }
+
+    // Returns the sum of the current and parameter Vector2.
+    add(vec2) {
+        if (vec2 instanceof Vector2) {
+            return new Vector2(
+                this.x + vec2.x,
+                this.y + vec2.y
+            );
+        }
+        
+        else {
+            console.log("A Vector2 was not passed as the parameter.");
+        }
+    }
+
+    // Returns the difference of the current and parameter Vector2.
+    subtract(vec2) {
+        if (vec2 instanceof Vector2) {
+            return new Vector2(
+                this.x - vec2.x,
+                this.y - vec2.y
+            );
+        }
+
+        else {
+            console.log("A Vector2 was not passed as the parameter.");
+        }
+    }
+
+    // Returns the dot product of the currecnt and parameter Vector2
+    dot(vec2) {
+        if (vec2 instanceof Vector2) {
+            return this.x * vec2.x + this.y * vec2.y;
+        }
+
+        else {
+            console.log("A Vector2 was not passed as the parameter.");
+        }
+    }
+
+    // Returns the distance between two vectors
+    dist(vec2) {
+        if (vec2 instanceof Vector2) {
+            return this.subtract(vec2).magnitude();
+        }
+
+        else {
+            console.log("A Vector2 was not passed as the parameter.");
+        }
+    }
+}
